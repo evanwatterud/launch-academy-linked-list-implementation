982009df71a73114469752fcba11eb6d8c978931
Evan Watterud <cod.videos@hotmail.com>
2016-05-18 01:19:54 -0400

    linked list functionality complete

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -2,5 +2,70 @@ require 'pry'
 require_relative 'node'
 
 class LinkedList
- 
+  def initialize
+    @head = nil
+  end
+
+  def prepend(value)
+    @head = Node.new(value, @head)
+  end
+
+  def [](index)
+    node = @head
+    index.times do
+      if node.next
+        node = node.next
+      else
+        return nil
+      end
+    end
+    return node
+  end
+
+  def each
+    node = @head
+
+    while !node.nil?
+      yield(node)
+      node = node.next
+    end
+  end
+
+  def to_s
+    return "LinkedList()" if @head == nil
+    string = "LinkedList("
+    node = @head
+    string << "#{node.data}"
+    node = node.next
+
+    while node
+      string << ", #{node.data}"
+      node = node.next
+    end
+
+    string << ")"
+    return string
+  end
+
+  def insert(index, value)
+    curr = self[index]
+
+    if index == 0
+      self.prepend(value)
+      return true
+    end
+
+    prev = self[index-1]
+    prev.next = Node.new(value, curr)
+  end
+
+  def remove(index)
+    if index == 0
+      @head = self[index].next
+      return true
+    end
+    prev = self[index-1]
+    next_node = self[index+1]
+    prev.next = next_node
+  end
 end
--- lib/node.rb
+++ lib/node.rb
@@ -1,2 +1,16 @@
 class Node
+  attr_accessor :data, :next
+
+  def initialize(data, next_node = nil)
+     self.data = data
+     self.next = next_node
+  end
+
+  def info
+    return self.data
+  end
+
+  def next_node
+    return self.next
+  end
 end
